cb6f8a16d1465a2b26fc77a8eed5cdc6d5a31bd3
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-04-05 23:12:03 +0200

    remove unused var

--- defaults.go
+++ defaults.go
@@ -1,19 +1,10 @@
 package examine
 
 import (
-	"github.com/go-delve/delve/pkg/proc"
 	"github.com/go-delve/delve/service/api"
 )
 
 var loadArgs = api.LoadConfig{
-	FollowPointers:     true,
-	MaxArrayValues:     10,
-	MaxStringLen:       100,
-	MaxStructFields:    10,
-	MaxVariableRecurse: 5,
-}
-
-var procLoadArgs = proc.LoadConfig{
 	FollowPointers:     true,
 	MaxArrayValues:     10,
 	MaxStringLen:       100,
