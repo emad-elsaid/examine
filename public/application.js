112dc07d74a12fad93be728e4607a3cd9e133c98
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-04-08 00:39:19 +0200

    add chart

--- public/application.js
+++ public/application.js
@@ -1,21 +1,20 @@
 Chart = G2.Chart
 
-const chart = new Chart({ container: "container" });
-
-chart.options({
-    type: "point",
-    height: 300,
-    data: {
-        type: "fetch",
-        value: "/timeline.json"
-    },
-    encode: { y: "Function", x: "Time", shape: "line", size: 10 },
-    transform: [{ type: "sortX", channel: "x" }],
-    scale: {
-        x: { type: "point" },
-        y: { },
-        color: { type: "ordinal" },
-    },
+const chart = new Chart({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    container: "container",
 });
+
+chart
+    .point()
+    .data({
+        type: 'fetch',
+        value: '/timeline.json',
+    })
+    .encode('x', 'Time')
+    .encode('y', 'Function')
+    .encode('color', 'File')
+    .axis('x', false)
 
 chart.render();
